2907b00c536c403149386298b5e4d6674b3dc52b
Mac Wimberly <maclain@gmail.com>
2015-12-09 23:02:42 -0600

    w02d03-hw

--- w02/d03/starter/deck_o_cards.js
+++ w02/d03/starter/deck_o_cards.js
@@ -1,38 +1,36 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays. 
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+//If you run this it will work and set up the deck of cards and do //the logs.
+// I messed around with the shuffle function for a while but
+//could not quite figure it out.
+
 function deck_o_cards() {
-	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  	var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
-	// Create the arrays for the deck, and shuffled deck
-	var cards = [];
-	// Make 52 card objects and store them in the "cards" array
+    var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+    var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    var cardDeck = [];
 
-	// Shuffle the Deck
+    for (v = 0, s = 0; s < 4; v++) {
 
-	// Pull the top card
+        var card = {
+            suit: suits[s],
+            value: values[v]
+        };
 
-	// Console log the results
-}
 
-// Fisher-Yates Shuffle 
-// http://stackoverflow.com/a/6274398
-function shuffle(array) {
-    var counter = array.length, temp, index;
+        if (v > 12) {
+            s++;
+            v = -1;
+        } else {
+            card.suit = suits[s];
+            card.value = values[v];
+            cardDeck.push(card);
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+        }
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+
     }
-
-    return array;
+    console.log(cardDeck);
+    console.log("The deck has " + cardDeck.length + " cards.")
+    console.log("The top card is the " + cardDeck[0].value + " of " + cardDeck[0].suit + ".");
 }
+deck_o_cards();
