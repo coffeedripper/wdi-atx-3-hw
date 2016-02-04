1b3cb2582c6d464eafa498209373cab3d59ace9d
Mac Wimberly <maclain@gmail.com>
2016-02-04 10:19:28 -0600

    hww08d03

--- w08/d03/starter/stargazer/js/stargazer.js
+++ w08/d03/starter/stargazer/js/stargazer.js
@@ -1,5 +1,30 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
-}
+// function search(name) {
+
+var api = 'http://www.strudel.org.uk/lookUP/json/?name=sun';
+
+$(function (){
+
+
+  $.ajax({
+   type: "GET",
+   url: api,
+   dataType: "jsonp",
+     success: function(response) {
+                    console.log(response);
+                    console.log(response.image.src);
+                    var pic = response.image.src;
+                     $('#preview').append('<img src=" ' + pic + '">');
+
+                }
+
+
+
+
+});
+
+
+});
+
+
+
+
